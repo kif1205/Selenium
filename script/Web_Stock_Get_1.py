d4ee53e20146bf10de981206c136381af21c1969
kif1205 <taylor1205@gmail.com>
2019-08-27 23:51:39 +0800

    Add files via upload

--- script/Web_Stock_Get_1.py
+++ script/Web_Stock_Get_1.py
@@ -1,5 +1,6 @@
 from Web_APIs import *
 from selenium.webdriver.common.keys import Keys
+import csv
 
 # The ways to copy path 
 # 1. Copy outer HTML => <input formcontrolname="kvmEnabled" name="chkKVM" type="checkbox" class="ng-valid ng-dirty ng-touched">
@@ -32,56 +33,6 @@ def Web_Open():
 	
 def Web_Close():
     Web_Test.close()
-
-
-
-def case1():
-    ###################
-    # Get LED's Color #
-    ###################
-    # Switch to Virtual Console tab
-    Web_Test.locate('//app-sidenav//li[8]/a')
-    Web_Test.click_button()
-    delays(3)
-    
-    # LED LIT Color
-    Web_Test.locate('/html/body/app-root/div/div/div[2]/app-remotecontrol/div/div[2]/div/div[1]/div/div[2]/div[1]/div[2]/app-toggleswitch/div/label[2]')    
-    test = Web_Test.get_css_property('background-color')
-    print(test)
-    
-    # LED OFF Color
-    Web_Test.locate('/html/body/app-root/div/div/div[2]/app-remotecontrol/div/div[2]/div/div[1]/div/div[2]/div[1]/div[2]/app-toggleswitch/div/label[1]')
-    test = Web_Test.get_css_property('background-color')
-    print(test)
-
-def case2():
-    #########################
-    # Get Toggle State Test #
-    #########################
-    # Switch to Power Management Tab
-    Web_Test.locate('//app-sidenav//li[6]/a')
-    Web_Test.click_button()
-    delays(3)
-    
-    Web_Test.locate('//app-power//app-accordion//app-accordion-group[1]/div')
-    test = Web_Test.get_single_attribute("class")
-    print(test)
-    # result : accordion-group => it means the tab is expaned
-    
-    Web_Test.locate('//app-power//app-accordion//app-accordion-group[2]/div')
-    test = Web_Test.get_single_attribute("class")
-    print(test)
-    # result : accordion-group closed => it means the tab is hided
-
-def case3():
-    Web_Open()
-
-    Web_Test.locate('//*[@id="tsf"]/div[2]/div/div[1]/div/div[1]/input')
-    
-    #for i in range(10):
-    #    Web_Test.send_keys(Keys.SPACE)
-    Web_Test.send_keys(Keys.F5)
-
     
 def main():
 
@@ -105,170 +56,15 @@ def main():
     for item in table:
         print(item)
 
-
+    with open("2330.csv", 'w') as myfile:
+        wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+        
+        for item in table:
+            wr.writerow(item)
     
-    # #######################
-    # # Change KVM Settings #
-    # #######################
-    # # Switch to Virtual Console tab
-    # Web_Test.locate("/html/body/app-root/div/div/div[1]/app-sidenav/div/ul/li[8]/a")
-    # Web_Test.click_button()
-    # delays(3)
-    # 
-    # # Clear KVM Port textbox
-    # Web_Test.locate("/html/body/app-root/div/div/div[2]/app-remotecontrol/div/div[2]/div/div[2]/app-vconsole/div/div[2]/form/div[3]/div/input")
-    # Web_Test.textbox_clear()
-    # 
-    # Web_Test.textbox_set("1111")
-    # 
-    # 
-    # # Get KVM Port
-    # Web_Test.locate("/html/body/app-root/div/div/div[2]/app-remotecontrol/div/div[2]/div/div[2]/app-vconsole/div/div[2]/form/div[3]/div/input")
-    # kvmport = Web_Test.textbox_get()
-    # print(kvmport)
-    
-    #
-    # # Get KVM Port label
-    # Web_Test.locate("/html/body/app-root/div/div/div[2]/app-remotecontrol/div/div[2]/div/div[2]/app-vconsole/div/div[2]/form/div[3]/label")
-    # kvmport_label = Web_Test.label_get()
-    # print(kvmport_label)
-    #
-    # # Get KVM Viewer button state
-    # Web_Test.locate("/html/body/app-root/div/div/div[2]/app-remotecontrol/div/div[2]/div/div[2]/app-vconsole/div/div[1]/div/div[2]/div/button[1]")
-    # kvm_viewer_button_state = Web_Test.get_button_state()
-    # print(kvm_viewer_button_state)
-
-    # # Change Boot Source Target
-    # Web_Test.locate("/html/body/app-root/div/div/div[2]/app-remotecontrol/div/div[2]/div/div[1]/app-boot/div/div[2]/div/div[2]/div[2]/select")
-    # Web_Test.dropdown_set(1)
-    # delays(3)
-    # Web_Test.dropdown_set(2)
-    # 
-    # # Get Boot Source Target
-    # Web_Test.locate("/html/body/app-root/div/div/div[2]/app-remotecontrol/div/div[2]/div/div[1]/app-boot/div/div[2]/div/div[2]/div[2]/select")
-    # bootsource = Web_Test.dropdown_get()
-    # print(bootsource)
-
-
-    # # Click KVM Enabled checkbox
-    # Web_Test.locate("//app-remotecontrol//app-vconsole//div[2]/form/div[1]//input")
-    # #Web_Test.locate("//input[@formcontrolname='kvmEnabled']")
-    # Web_Test.checkbox_click()
-    # 
-    # #Web_Test.locate("/html/body/app-root/div/div/div[2]/app-remotecontrol/div/div[2]/div/div[2]/app-vconsole/div/div[2]/form/div[1]/div/input")
-    # checkbox_state =  Web_Test.checkbox_get()  
-    # print(checkbox_state)  
-    
-    
-    # # Open KVM Viewer 
-    # Web_Test.locate("/html/body/app-root/div/div/div[2]/app-remotecontrol/div/div[2]/div/div[2]/app-vconsole/div/div[1]/div/div[2]/div/button[1]")
-    # Web_Test.click_button()
-    # delays(5)
-    # 
-    # # Switch to KVM Viewer and get the title
-    # Web_Test.switch_window(1) 
-    # window_1_title = Web_Test.get_window_title(1)
-    # print(window_1_title)
-    # 
-    # # Get the current Available VM Menu
-    # Web_Test.locate('//*[@id="navbarDropdownMenuVMedia"]')
-    # Web_Test.click_button()
-    # delays(3)
-    # 
-    # Web_Test.locate('//*[@id="navbarSupportedContent"]/ul/li[5]/div')
-    # VM_Menu = Web_Test.get_single_attribute("outerText")
-    # delays(1)
-    # 
-    # VM_Menu = VM_Menu.split("\n")
-    # print(VM_Menu)
-    # 
-    # # Close KVM Viewer
-    # Web_Test.close_window(1) 
-    # 
-    # # Switch to Main Window and get the title
-    # Web_Test.switch_window(0)
-    # window_0_title = Web_Test.get_window_title(0)
-    # print(window_0_title)
-    
-
-
-    # ########################
-    # # Change BIOS Settings #
-    # ########################
-    # # Switch to BIOS Settings tab
-    # Web_Test.locate("/html/body/app-root/div/div/div[1]/app-sidenav/div/ul/li[9]/a")
-    # Web_Test.click_button()
-    # delays(3)
-    # 
-    # Web_Test.locate('//*[@id="navbarDropdownMenuFile"]')
-    # Web_Test.click_button()
-    # delays(2)
-    # 
-    # Web_Test.locate('//*[@id="navbarSupportedContent"]/ul/li/div/div[3]/div/ul/li/a')    
-    # Web_Test.move_cursor_onto()
-    # delays(1)
-    # 
-    # Web_Test.locate('//*[@id="navbarSupportedContent"]/ul/li/div/div[3]/div/ul/li/div/ul/li/a') 
-    # Web_Test.click_button()
-    # delays(1)
-    
-    # ###############
-    # # Dialog Test #
-    # ###############
-    # # Test Delete SEL Logs
-    # # Switch to Logs Settings tab
-    # Web_Test.locate("/html/body/app-root/div/div/div[1]/app-sidenav/div/ul/li[3]/a")
-    # Web_Test.click_button()
-    # delays(3)
-    # 
-    # # Click the Delete SEL Button
-    # Web_Test.locate('/html/body/app-root/div/div/div[2]/app-logs/div/div[2]/div/div/div/app-eventlog/div/div/div/div[1]/div[2]/div[1]/a/i')
-    # Web_Test.click_button()
-    # delays(3)
-    #     
-    # Web_Test.locate("/html/body/app-root/div/div/div[2]/app-message-dialog/app-message-modal/div/div/div/div[2]/div/div/div")
-    # test_dialog_1 = Web_Test.label_get()
-    # print(test_dialog_1)
-    # 
-    # Web_Test.locate("/html/body/app-root/div/div/div[2]/app-message-dialog/app-message-modal/div/div/div/div[3]/div/button[2]")
-    # Web_Test.click_button()
-    # delays(3)    
-    # 
-    # Web_Test.locate('//*[@id="extendedInfoDialog"]/div/div[2]/div/div/div/div')
-    # test_dialog_2 = Web_Test.label_get()
-    # print(test_dialog_2)
-    # delays(3)     
-    # 
-    # Web_Test.locate('//*[@id="extendedInfoDialog"]/div/div[3]/div/button')
-    # Web_Test.click_button()
-    # delays(3)     
-    
-    # ##############################
-    # # Firmware Update management #
-    # ##############################
-    # # Switch to Firmware tab
-    # Web_Test.locate("//app-sidenav/div/ul/li[7]/a")
-    # Web_Test.click_button()
-    # #delays(3)
-    # 
-    # Web_Test.locate("//input[@name='txtTFTPPath']")
-    # Web_Test.textbox_clear()
-    # #delays(3)
-    # 
-    # Web_Test.locate("//input[@name='txtTFTPPath']")
-    # Web_Test.textbox_set("tftp://1.1.1.1")
-    # #delays(3)
-    # 
-    # Web_Test.locate("//div[@class='accordion-group']//fieldset[@class='scheduler-border']//button[@class='btn btn-vertiv']")
-    # Web_Test.click_button()
-    # #delays(3)    
-    # # 
-    # Web_Test.locate("//div[contains(@class, 'accordion-group')]//fieldset[contains(@class,'scheduler-border')]//div[contains(@class,'col-md-6')]//div[contains(@class,'font-italic font-weight-bold')]")
-    # test = Web_Test.label_get()
-    # # #delays(3)  
-    # print(test)
-    # 
     #Web_Test.refresh()
+    
+    
     #Web_Close()
     
 if __name__ == "__main__":
